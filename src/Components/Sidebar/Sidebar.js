5fe6334a80cadc7b68747af1d4ec7e3214945f01
Vlasya <albina61446@gmail.com>
2021-01-05 12:59:00 +0200

    'added_sidebar'

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ import {NavLink} from "react-router-dom";
 const Sidebar = () => {
     return (
         <div className={s.wrapper}>
-            <NavLink className={s.nav_link} to="/profile" activeClassName={s.active}>Profile</NavLink>
+            <NavLink className={s.nav_link}  to="/profile" activeClassName={s.active}>Profile</NavLink>
             <NavLink className={s.nav_link} to="/dialogs" activeClassName={s.active}>Messages</NavLink>
             <NavLink className={s.nav_link} to="/news" activeClassName={s.active}>News</NavLink>
             <NavLink className={s.nav_link} to="/music" activeClassName={s.active}>Music</NavLink>
